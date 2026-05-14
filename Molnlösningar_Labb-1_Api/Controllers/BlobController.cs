0dbd3ff54ea23834b2c74ded1965a17a843e5da1
william nilsson <williamnilsson1129@gmail.com>
2026-05-14 15:03:05 +0200

    Added blob features

---
+++ Molnlösningar_Labb-1_Api/Controllers/BlobController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Molnlösningar_Labb_1_Api.Core.Interfaces;
+
+namespace Molnlösningar_Labb_1_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobController : ControllerBase
+    {
+        private readonly IBlobService _service;
+
+        public BlobController(IBlobService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet("files")]
+        public async Task<IActionResult> GetFiles()
+        {
+            var files = await _service.GetAllFilesAsync();
+            return Ok(files);
+        }
+    }
+}
