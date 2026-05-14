e7971a34e661b4e703f31bdc72908a1b526d48d4
william nilsson <williamnilsson1129@gmail.com>
2026-05-14 15:03:05 +0200

    Added blob features

--- Molnlösningar_Labb-1_Api/Program.cs
+++ Molnlösningar_Labb-1_Api/Program.cs
@@ -1,3 +1,7 @@
+using Azure.Identity;
+using Molnlösningar_Labb_1_Api.Core.Interfaces;
+using Molnlösningar_Labb_1_Api.Core.Services;
+
 namespace Molnlösningar_Labb_1_Api
 {
     public class Program
@@ -5,8 +9,27 @@ namespace Molnlösningar_Labb_1_Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+            var keyVaultUri = builder.Configuration["KeyVaultUri"];
+
+            if (!string.IsNullOrEmpty(keyVaultUri))
+            {
+                builder.Configuration.AddAzureKeyVault(
+                    new Uri(keyVaultUri),
+                    new DefaultAzureCredential());
+            }
+
+            var storageConn = builder.Configuration["StorageConnectionString"];
+
+            var conn = builder.Configuration["DbConnectionString"];
+
+            builder.Services.AddScoped<IBlobService, BlobService>();
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
+            app.MapControllers();
 
             app.Run();
         }
